0b8c71a2b654c4bd80714226cf03668f19aab2fb
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-06-09 21:16:52 +0200

    Add files via upload

---
+++ Aplikacija za unos i obradu podataka/Aplikacija za unos i obradu podataka/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Aplikacija_za_unos_i_obradu_podataka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnupid = new System.Windows.Forms.Button();
+            this.btnispis = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnupid
+            // 
+            this.btnupid.Location = new System.Drawing.Point(79, 171);
+            this.btnupid.Name = "btnupid";
+            this.btnupid.Size = new System.Drawing.Size(102, 36);
+            this.btnupid.TabIndex = 0;
+            this.btnupid.Text = "Upis";
+            this.btnupid.UseVisualStyleBackColor = true;
+            this.btnupid.Click += new System.EventHandler(this.btnupid_Click);
+            // 
+            // btnispis
+            // 
+            this.btnispis.Location = new System.Drawing.Point(303, 241);
+            this.btnispis.Name = "btnispis";
+            this.btnispis.Size = new System.Drawing.Size(102, 34);
+            this.btnispis.TabIndex = 1;
+            this.btnispis.Text = "Ispis";
+            this.btnispis.UseVisualStyleBackColor = true;
+            this.btnispis.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(79, 127);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(232, 63);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(247, 144);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Marka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Vrsta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 306);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnispis);
+            this.Controls.Add(this.btnupid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnupid;
+        private System.Windows.Forms.Button btnispis;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Aplikacija za unos i obradu podataka/Aplikacija za unos i obradu podataka/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Aplikacija_za_unos_i_obradu_podataka
+{
+    public partial class Form1 : Form
+
+
+
+    {
+
+
+        List<Vozilo> vozila = new List<Vozilo>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("Avion");
+            comboBox1.Items.Add("Automobil");
+            comboBox1.Items.Add("Brod");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string message = "";
+
+            foreach (Vozilo vozilo in vozila)
+            {
+                message += $"Marka: {vozilo.Marka}, Model: {vozilo.Model}, " +
+                    $"Vrsta: {vozilo.Vrsta}, Vozi po: {vozilo.VoziPo}";
+            }
+            richTextBox1.Text = message;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnupid_Click(object sender, EventArgs e)
+        {
+            string marka = textBox1.Text;
+            string model = textBox2.Text;
+            string vrsta = comboBox1.SelectedItem.ToString();
+
+            Vozilo novoVozilo = new Vozilo(marka, model, vrsta);
+            vozila.Add(novoVozilo);
+
+            foreach (Vozilo vozilo in vozila)
+            {
+                if (vozilo.Vrsta == "Avion")
+                {
+                    vozilo.VoziPo = "Zrak";
+                }
+                else if (vozilo.Vrsta == "Automobil")
+                {
+                    vozilo.VoziPo = "Cesta";
+                }
+                else if (vozilo.Vrsta == "Brod")
+                {
+                    vozilo.VoziPo = "Voda";
+                }
+            }
+        }
+
+        public class Vozilo
+        {
+            public string Marka { get; set; }
+            public string Model { get; set; }
+            public string Vrsta { get; set; }
+            public string VoziPo { get; set; }
+
+            public Vozilo(string marka, string model, string vrsta)
+            {
+                Marka = marka;
+                Model = model;
+                Vrsta = vrsta;
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+    }
+}
